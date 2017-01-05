873fb20968046b5aa0e3ede072bcb8e5b241e10a
tejas Pashte <sulkytejas@yahoo.co.in>
2017-01-05 14:01:34 -0500

    minor

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ActiveRecord::Base
       'html' => 'Gift Card<br>$20',
       'class' => 'three',
       'image' => ActionController::Base.helpers.asset_path(
-        'refer/gf_two.png')
+        'refer/truman@2x.png')
     },
     {
       'count' => 25,
