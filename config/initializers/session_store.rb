fc28d10d0e22748f2c9fcc688c17a56a3533d79a
tejas Pashte <sulkytejas@yahoo.co.in>
2017-01-12 10:48:31 -0500

    database enabled

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Prelaunchr::Application.config.session_store :cookie_store, key: '_prelaunchr_session'
+#Prelaunchr::Application.config.session_store :cookie_store, key: '_prelaunchr_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
