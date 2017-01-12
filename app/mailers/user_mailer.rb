f41167f04b33e4776ab643e9d8bd48289fe99504
tejas Pashte <sulkytejas@yahoo.co.in>
2017-01-12 14:27:28 -0500

    updated mailerRB,secrets,applicationerb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "Printed Village's <designers.printedvillage.com>"
+  default from: "Printed Village's <tejas@printedvillage.com>"
 
   def signup_email(user)
     @user = user
